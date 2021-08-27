8da8d89723492b65da6ce60d73894c818c6e3b75
Ramiro Melero <ramiromelero@gmail.com>
2021-08-27 15:01:56 -0300

    Update echoOutput.sh

--- echoOutput.sh
+++ echoOutput.sh
@@ -3,3 +3,4 @@
 echo "Envío salida de echo desde testBash.sh hacia file.txt" > ./file.txt
 
 # Al ejecutar se crea el archivo file.txt y guarda la salida de echo en dicho archivo.
+# Si el archivo file.txt existe lo sobreescribe.
